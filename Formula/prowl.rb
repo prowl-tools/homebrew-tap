e4853c5d2604865aafa3fe96f0754f2b6dfd8363
Prowl Tools <no-reply@prowl.tools>
2026-08-17 16:58:57 -0500

    prowl 0.1.4

--- Formula/prowl.rb
+++ Formula/prowl.rb
@@ -1,8 +1,8 @@
 class Prowl < Formula
   desc "CLI-first end-to-end testing tool using Playwright for browser automation"
   homepage "https://prowl.tools"
-  url "https://registry.npmjs.org/prowl-tools/-/prowl-tools-0.1.3.tgz"
-  sha256 "d23174b0013d88b6e00cf37702aa2ac62ac2c1b72d6f134f347017b9ebaf6584"
+  url "https://registry.npmjs.org/prowl-tools/-/prowl-tools-0.1.4.tgz"
+  sha256 "0dbfc4604f0970a16435914b8856b9dacb743e8c3bacdf5511a21b60e775b618"
   license "Apache-2.0"
 
   depends_on "node@20"
