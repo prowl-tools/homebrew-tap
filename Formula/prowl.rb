73bac92e3f5606bb490d37aa5e12185a270e4dd1
Prowl Tools <no-reply@prowl.tools>
2026-09-08 16:48:07 -0500

    prowl 0.1.8

--- Formula/prowl.rb
+++ Formula/prowl.rb
@@ -1,8 +1,8 @@
 class Prowl < Formula
   desc "E2E testing for native macOS apps and web apps from declarative YAML hunts"
   homepage "https://prowl.tools"
-  url "https://registry.npmjs.org/prowl-tools/-/prowl-tools-0.1.7.tgz"
-  sha256 "4d7be4942e261812c6f5aae2b2ea609e90a46f9172510f6925ff85abd5389173"
+  url "https://registry.npmjs.org/prowl-tools/-/prowl-tools-0.1.8.tgz"
+  sha256 "5f291e29d67ca85697259557dfee701cd7a114ff3dacdc3694812d005de802fc"
   license "Apache-2.0"
 
   depends_on "node@20"
